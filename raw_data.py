8f5908de1019ddf66e24460ed243f3c666099a59
Yusuf-Sayeed <yusufsayeed1706@gmail.com>
2026-07-17 23:51:56 +0530

    Add Regression df

--- raw_data.py
+++ raw_data.py
@@ -5,19 +5,28 @@ import pandas as pd
 raw_data = pd.read_excel("Auctions_of_91_Day_Government_of_India_Treasury_Bills.xlsx")
 
 # %%
+raw_data = raw_data[raw_data.iloc[:, 3] != 0].reset_index(drop=True)
+
+
+# %%
 r = raw_data.iloc[:, 3] / 100
 r.describe()
 
 # %%
-raw_data = raw_data[raw_data.iloc[:, 3] != 0].reset_index(drop=True)
-
-# %%
 dates = raw_data.iloc[:, 0]
 dt = dates.diff().dt.days / 365
-dt.dropna(how = any, inplace = True)
 dt.describe()
 
 # %%
 dr = r.diff()
-dr.dropna(how = any, inplace = True)
+dr.describe()
+
 # %%
+r_prev = r.shift(1)
+r_prev
+
+# %%
+# %%
+reg_data = pd.DataFrame({'dt': dt, 'dr': dr, 'r_prev': r_prev})
+reg_data = reg_data.dropna().reset_index(drop=True)
+reg_data
