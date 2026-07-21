9d32bd3c7c65d5095dac6080f7e554d15bafd515
Yusuf-Sayeed <yusufsayeed1706@gmail.com>
2026-07-21 20:14:17 +0530

    Add next period nodes

--- vasicek__tree.py
+++ vasicek__tree.py
@@ -16,3 +16,15 @@ r_down = r0 + (k*(theta - r0)*dt_tree - sigma*np.sqrt(dt_tree))
 print(f"r_up : {r_up} \nr_down : {r_down}")
 
 # %%
+r_uu = r_up + (k*(theta - r_up)*dt_tree + sigma*np.sqrt(dt_tree))
+r_ud = r_up + (k*(theta - r_up)*dt_tree - sigma*np.sqrt(dt_tree))
+
+r_du = r_down + (k*(theta - r_down)*dt_tree + sigma*np.sqrt(dt_tree))
+r_dd = r_down + (k*(theta - r_down)*dt_tree - sigma*np.sqrt(dt_tree))
+
+print(f"r_uu : {r_uu} \nr_ud : {r_ud} \nr_du : {r_du} \nr_dd : {r_dd}")
+
+# %%
+r_mid = (r_ud + r_du).mean()
+
+# %%
