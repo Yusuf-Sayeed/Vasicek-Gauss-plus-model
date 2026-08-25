7c2975c1489edc3ee53821cb9eadc0fd48235319
Yusuf-Sayeed <yusufsayeed1706@gmail.com>
2026-08-25 22:02:47 +0530

    Add recombining feature

--- vasicek__tree.py
+++ vasicek__tree.py
@@ -25,6 +25,26 @@ r_dd = r_down + (k*(theta - r_down)*dt_tree - sigma*np.sqrt(dt_tree))
 print(f"r_uu : {r_uu} \nr_ud : {r_ud} \nr_du : {r_du} \nr_dd : {r_dd}")
 
 # %%
-r_mid = (r_ud + r_du).mean()
+r_mid = (r_ud + r_du) / 2 
+r_mid
 
+# %%
+M1 = 0.5 * (r_uu + r_ud)
+M2 = 0.5 * (r_uu**2 + r_ud**2)
+V = M2 - M1**2
+V
+# %%
+p = (M1 - r_mid)**2 / (V + (M1 - r_mid)**2)
+p
+# %%
+r_uu_new = r_mid + ((M1 - r_mid)/p)
+r_uu_new
+# %%
+M1_lower = 0.5 * (r_du + r_dd)
+M2_lower = 0.5 * (r_du**2 + r_dd**2)
+V_lower = M2_lower - M1_lower**2
+
+q = (M1_lower - r_mid)**2 / (V_lower + (M1_lower - r_mid)**2)
+r_dd_new = r_mid + (M1_lower - r_mid) / q
+r_dd_new
 # %%
